34c42942c8b7b49adf7bd97256f9de7849526cf3
Hanna <hannaostling@hotmail.se>
2017-11-13 23:31:41 +0100

    Revert "Layout"
    This reverts commit 05c3b7fd5c8f946189aff4a724ef8115e13575d9.

---
+++ app/src/main/java/case2/iths/com/case2/Test3Activity.java
@@ -0,0 +1,13 @@
+package case2.iths.com.case2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Test3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
