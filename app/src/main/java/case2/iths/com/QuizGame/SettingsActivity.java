484b264e09288e5e52b0b31303207fa9f31b4dba
kristofferknape <Kristoffer.knape@gmail.com>
2017-11-14 11:34:16 +0100

    deleted uneccesery stuff

--- app/src/main/java/case2/iths/com/QuizGame/SettingsActivity.java
+++ app/src/main/java/case2/iths/com/QuizGame/SettingsActivity.java
@@ -1,14 +1,13 @@
-package case2.iths.com.QuizGame.LayoutExamples;
+package case2.iths.com.QuizGame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import case2.iths.com.QuizGame.R;
 
-public class LayoutExampleActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test5);
+        setContentView(R.layout.activity_settings);
     }
 }
