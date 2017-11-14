73a1f9a75b2f1231d311b102ec424e9a6a5b8e3c
Hanna <hannaostling@hotmail.se>
2017-11-14 16:48:16 +0100

    Strings i layout

--- app/src/main/java/case2/iths/com/QuizGame/SplashActivity.java
+++ app/src/main/java/case2/iths/com/QuizGame/SplashActivity.java
@@ -15,6 +15,8 @@ public class SplashActivity extends AppCompatActivity {
 
         new Handler().postDelayed(new Runnable() {
 
+            //HEJ
+
             @Override
             public void run() {
                 Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
