ebee082f9116fc48d0fdbd68eb632a66f2400f41
Hanna <hannaostling@hotmail.se>
2017-11-15 13:01:14 +0100

    DPHelper.java Highscore.java HadnleQuesionsActivity.java activity_handle:questions

--- app/src/main/java/case2/iths/com/QuizGame/HandleQuestionsActivity.java
+++ app/src/main/java/case2/iths/com/QuizGame/HandleQuestionsActivity.java
@@ -1,23 +1,53 @@
 package case2.iths.com.QuizGame;
 
+// TODO: 2017-11-14 Lägg till:
+// TODO: CREATE QUESTION-FUNCTION
+// TODO:        ADD QUESTION
+// TODO:        ADD ALTERNITIVES
+// TODO:           - WRONG ANSWERS
+// TODO:           - RIGHT ANSWERS
+// TODO:        ADD CATEGORY
+// TODO: DELETE QUESTION-FUNCTION
+// TODO: RANKING
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 public class HandleQuestionsActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList<String> list;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handle_questions);
+        listView = findViewById(R.id.listView);
+
+        //Data källa för vår listview
+        list = new ArrayList<>();
+        list.add("Text 1");
+        list.add("Text 2");
+        list.add("Text 3");
+
+        //Färdig adapter för ListView
+        arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, list);
+
+        //Connect listView to arrayAdapter
+        listView.setAdapter(arrayAdapter);
+
+
     }
 
-    // TODO: 2017-11-14 Lägg till:
-    // TODO: CREATE QUESTION-FUNCTION
-    // TODO:        ADD QUESTION
-    // TODO:        ADD ALTERNITIVES
-    // TODO:           - WRONG ANSWERS
-    // TODO:           - RIGHT ANSWERS
-    // TODO:        ADD CATEGORY
-    // TODO: DELETE QUESTION-FUNCTION
-    // TODO: RANKING
+    public void onButtonClick(View v) {
+
+        arrayAdapter.add("Text");
+
+    }
 }
---
+++ app/src/main/java/case2/iths/com/QuizGame/Highscore.java
@@ -0,0 +1,13 @@
+package case2.iths.com.QuizGame;
+
+/**
+ * Created by Hanna on 2017-11-15.
+ */
+
+class HighScore {
+
+    public long id;
+    public String name;
+    public int points;
+
+}
