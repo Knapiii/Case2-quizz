1b1a6958219665d04a831e867ddf387442c93f32
kristofferknape <Kristoffer.knape@gmail.com>
2017-11-16 10:34:27 +0100

    add: in settingsActivity - working toggle mute sound for buttons

--- app/src/main/java/case2/iths/com/QuizGame/SettingsActivity.java
+++ app/src/main/java/case2/iths/com/QuizGame/SettingsActivity.java
@@ -1,6 +1,5 @@
 package case2.iths.com.QuizGame;
 
-import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
@@ -10,7 +9,7 @@ import android.widget.ImageButton;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private boolean soundOn = true;
+    private boolean soundOn = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,8 +19,6 @@ public class SettingsActivity extends AppCompatActivity {
 
     public void testSoundButton(View view) {
         isSoundOn();
-        Intent testSoundButton = new Intent(this,SettingsActivity.class);
-        startActivity(testSoundButton);
     }
 
     public void toggleSoundOnClick(View view) {
@@ -32,23 +29,19 @@ public class SettingsActivity extends AppCompatActivity {
             soundOn = false;
             icon = R.drawable.muted_sound;
             //Ljud av
-
-
         } else {
             soundOn = true;
             icon = R.drawable.unmuted_sound;
-            //Ljud på
-
         }
         button.setImageDrawable(
                 ContextCompat.getDrawable(getApplicationContext(), icon));
     }
 
     public boolean isSoundOn() {
-
-
-        MediaPlayer mMediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.button_click_sound);
-        mMediaPlayer.start();
+        if (!soundOn) {
+            MediaPlayer mMediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.button_click_sound);
+            mMediaPlayer.start();
+        }
         return soundOn;
     }
 
