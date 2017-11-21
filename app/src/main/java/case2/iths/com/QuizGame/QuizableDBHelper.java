b4a6bd2839dcc494cb125a1e3f01d363ecdc0b31
DrTails <33480190+DrTails@users.noreply.github.com>
2017-11-21 15:20:41 +0100

    Added QuizableDBHelper - Now able to read QuizableDB.db located in assets/databases. Added SQLiteAssetHelper in build.gradle - Like SQLiteOpenHelper, but works with files in assets/databases folder.

---
+++ app/src/main/java/case2/iths/com/QuizGame/QuizableDBHelper.java
@@ -0,0 +1,35 @@
+package case2.iths.com.QuizGame;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+/**
+ * Created by Daniel on 2017-11-20.
+ */
+
+public class QuizableDBHelper extends SQLiteAssetHelper {
+
+    private static final String TABLE = "Questions";
+    private static final String KEY_ID = "id";
+    private static final String CATEGORY = "Category";
+    private static final String QUESTION = "Question";
+    private static final String ANSWER = "Answer";
+    private static final String DATABASE_NAME = "QuizableDB.db";
+    private static final int DATABASE_VERSION = 1;
+
+
+    public QuizableDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public Cursor getQuestions() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE;
+        Cursor cursor = db.rawQuery(query, null);
+        return cursor;
+    }
+
+}
