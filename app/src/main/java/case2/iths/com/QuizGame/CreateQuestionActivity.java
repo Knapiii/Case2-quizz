7515b71937ed33206d308bdf732f398abbf66847
Robert <roberteriksson12@gmail.com>
2017-11-21 12:00:15 +0100

    createQuestions margins fixed

--- app/src/main/java/case2/iths/com/QuizGame/CreateQuestionActivity.java
+++ app/src/main/java/case2/iths/com/QuizGame/CreateQuestionActivity.java
@@ -2,7 +2,9 @@ package case2.iths.com.QuizGame;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Spinner;
+import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 
@@ -11,12 +13,16 @@ public class CreateQuestionActivity extends AppCompatActivity {
     public Spinner spinner;
     public String[] cats;
     private ArrayList<String> categories = new ArrayList<>();
+    private ToggleButton buttonTrue;
+    private ToggleButton buttonFalse;
+    private boolean buttonTrueClicked;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_question);
+        buttonTrueClicked = false;
 
         categories.add("Sport");
         categories.add("Food");
@@ -31,6 +37,10 @@ public class CreateQuestionActivity extends AppCompatActivity {
         spinner = findViewById(R.id.spinner_add_category);
         HighscoresAdapter addToCategoryAdapter = new HighscoresAdapter(this, categories);
         spinner.setAdapter(addToCategoryAdapter);
+
+    }
+
+    public void onButtonClicked(View view){
 
     }
 }
