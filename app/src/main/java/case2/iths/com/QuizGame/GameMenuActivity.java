a03f5750803281aa90e218d8d27bdb04ff1c9b64
Alvar <aronija_1@hotmail.com>
2017-12-04 09:39:13 +0100

    You can now select a Profile from GameMenuActivity

--- app/src/main/java/case2/iths/com/QuizGame/GameMenuActivity.java
+++ app/src/main/java/case2/iths/com/QuizGame/GameMenuActivity.java
@@ -1,20 +1,38 @@
 package case2.iths.com.QuizGame;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Spinner;
 
 public class GameMenuActivity extends AppCompatActivity {
 
     private SavedSettings savedSettings;
+    private Spinner mProfilesSpinner;
+    private QuizableOpenHelper mDbOpenHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_menu);
         savedSettings = new SavedSettings();
+
+        mDbOpenHelper = new QuizableOpenHelper(this);
+        displayProfilesSpinner();
+
+
     }
+
+    private void displayProfilesSpinner() {
+
+        mProfilesSpinner = findViewById(R.id.spinner_profiles);
+        Cursor allProfiles = mDbOpenHelper.getAllProfiles();
+        ProfilesCursorAdapter profilesCursorAdapter = new ProfilesCursorAdapter(this, allProfiles, 0);
+        mProfilesSpinner.setAdapter(profilesCursorAdapter);
+    }
+
 
     /**
      * När vi klickar på knappen "Manage Statements" så ska vi komma till HandleStatementsActivity
@@ -46,4 +64,9 @@ public class GameMenuActivity extends AppCompatActivity {
         startActivity(toMultiplayerGame);
     }
 
+    @Override
+    protected void onDestroy() {
+        mDbOpenHelper.close();
+        super.onDestroy();
+    }
 }
---
+++ app/src/main/java/case2/iths/com/QuizGame/ProfilesCursorAdapter.java
@@ -0,0 +1,34 @@
+package case2.iths.com.QuizGame;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import case2.iths.com.QuizGame.QuizableDatabaseContract.UserInfoEntry;
+
+/**
+ * Created by alvaro on 2017-12-03.
+ */
+
+public class ProfilesCursorAdapter extends CursorAdapter {
+    public ProfilesCursorAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.dropdown_list_item, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView textView = view.findViewById(R.id.textView);
+        String userName = cursor.getString(cursor.getColumnIndex(UserInfoEntry.COLUMN_USERNAME));
+        textView.setText(userName);
+
+    }
+}
