48995089cbf9c052c9b47ed7b6a0ba4059683ee6
Hanna <hannaostling@hotmail.se>
2017-12-06 00:31:17 +0100

    Javakonventioner för MultiplayerActivity.

--- app/src/main/java/case2/iths/com/QuizGame/MultiplayerActivity.java
+++ app/src/main/java/case2/iths/com/QuizGame/MultiplayerActivity.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 public class MultiplayerActivity extends AppCompatActivity {
 
     private SavedSettings savedSettings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiplayer);
         savedSettings = new SavedSettings();
     }
+
 }
