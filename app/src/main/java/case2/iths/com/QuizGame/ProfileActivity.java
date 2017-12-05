855446bc4be512f518f1010485fc3050ce43f45b
Hanna <hannaostling@hotmail.se>
2017-12-06 00:32:42 +0100

    Javadocs för ProfileActivity. Javakonventioner.

--- app/src/main/java/case2/iths/com/QuizGame/ProfileActivity.java
+++ app/src/main/java/case2/iths/com/QuizGame/ProfileActivity.java
@@ -20,32 +20,25 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         savedSettings = new SavedSettings();
-
         mDbOpenHelper = new QuizableOpenHelper(this);
         profilesCursor = mDbOpenHelper.getAllProfiles();
-
         displayProfiles(profilesCursor);
-
     }
 
     private void displayProfiles(Cursor profilesCursor) {
-
         recyclerView = findViewById(R.id.list_profiles);
         LinearLayoutManager profilesLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(profilesLayoutManager);
-
         ProfileAdapter profileAdapter = new ProfileAdapter(this, profilesCursor);
         recyclerView.setAdapter(profileAdapter);
-
-
     }
 
+    /**
+     * När vi klickar på addButton ska vi komma till CreateProfileActivity
+     */
     public void onAddButtonClick(View view) {
-
         Intent intent = new Intent(this, CreateProfileActivity.class);
         startActivity(intent);
-
-
     }
 
     @Override
@@ -53,8 +46,5 @@ public class ProfileActivity extends AppCompatActivity {
         super.onDestroy();
         mDbOpenHelper.close();
     }
-// TODO: 2017-11-14 Lägg till:
-    // TODO:
-    // TODO:
-    // TODO:
+
 }
