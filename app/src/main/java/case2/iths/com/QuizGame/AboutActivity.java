e0ec3c243ac7d354f2c8887564e54a79a30869e6
Hanna <hannaostling@hotmail.se>
2017-12-11 09:34:09 +0100

    Tog bort savedSettings då det inte behövdes i den här aktiviteten.

--- app/src/main/java/case2/iths/com/QuizGame/AboutActivity.java
+++ app/src/main/java/case2/iths/com/QuizGame/AboutActivity.java
@@ -7,15 +7,14 @@ import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
-    private SavedSettings savedSettings;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        savedSettings = new SavedSettings();
         TextView textview = (TextView) findViewById(R.id.about_text);
         textview.setMovementMethod(new ScrollingMovementMethod());
     }
 
 }
+
+
